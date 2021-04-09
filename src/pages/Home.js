122a723e4e8b8b66427b597aa77e67cd66322dda
nvard20 <nvard.asatryan96@gmail.com>
2021-04-09 15:55:00 +0400

    all

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+          <div className='box-img'>
+            <img src='img/img-1.png' />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
