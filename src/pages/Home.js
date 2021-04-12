3d2528aca319f82f7217528fd3fcbf1a6bdc7733
nvard20 <nvard.asatryan96@gmail.com>
2021-04-12 14:08:14 +0400

    all

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './img/img-1.png';
 
 const Home = () => {
     return (
         <div>
           <div className='box-img'>
               <h1>Hi everyone</h1>
-             <img src={logo}  />
+        
             </div>
         </div>
     );
