2cfa7b2d51cd8cbedb83148ea39d4bc4e70a4165
nvard20 <nvard.asatryan96@gmail.com>
2021-04-12 12:22:55 +0400

    Create a React app and publish it to GitHub Pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from './img/img-1.png';
 
 const Home = () => {
     return (
         <div>
           <div className='box-img'>
-            <img src='img/img-1.png' />
+              <h1>Hi everyone</h1>
+             <img src={logo}  />
             </div>
         </div>
     );
